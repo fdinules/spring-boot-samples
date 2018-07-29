272ced79dec35da8c4015ba2a6feb960f0c21a5c
fdinules <florin.dinulescu@yahoo.com>
2018-07-29 21:48:17 +0300

    use Message<SpecificCommand> instead of Message<Command>

--- spring-boot-sample-rabbitmq/consumer/src/main/java/com/example/consumer/handler/SimpleMessageHandler.java
+++ spring-boot-sample-rabbitmq/consumer/src/main/java/com/example/consumer/handler/SimpleMessageHandler.java
@@ -5,13 +5,13 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
 
 import com.example.consumer.sink.Sink;
-import com.example.message.library.Command;
+import com.example.message.library.SpecificCommand;
 
 @EnableBinding(Sink.class)
 public class SimpleMessageHandler {
 
 	@StreamListener(Sink.COMMAND_PRODUCER_TO_CONSUMER)
-	public void handle(Message<? extends Command> cmd) {
-		System.err.println("Received: " + cmd);
+	public void handle(Message<SpecificCommand> cmd) {
+		System.err.println("*** Received SpecificCommand: " + ((SpecificCommand) cmd.getPayload()).getCommandSpecificId());
 	}
 }
